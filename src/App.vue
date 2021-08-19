c95f779194fa9e937b8398822382fbbf9385764e
Longino Bigage <longinobigage@live.com>
2021-08-19 16:50:00 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <header class="sticky top-0 h-16 bg-green-600"></header>
+  <div class="h-screen bg-red-600"></div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-</style>
+</script>
