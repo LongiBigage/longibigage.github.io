d144a25e58707110280c79b0d6efc1db62b97947
Longino Bigage <longinobigage@live.com>
2021-08-19 16:50:20 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/longibigage.github.io/'
+    : '/',
   plugins: [vue()]
 })
