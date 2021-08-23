fca13e0d2d1bcb762ded31f50a9317d251a9df4b
Longino Bigage <longinobigage@live.com>
2021-08-23 13:18:24 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          900: "hsl(328, 68%, 32%)",
+          700: "hsl(328, 72%, 35%)",
+          500: "hsl(328, 43%, 39%)"
+        }
+      },
+      fontFamily: {
+        montserrat: ["Montserrat, sans-serif"],
+        poppins: ["Poppins, sans-serif"],
+        conforta: ["Comfortaa, cursive"]
+      }
+    },
   },
   variants: {
     extend: {},
