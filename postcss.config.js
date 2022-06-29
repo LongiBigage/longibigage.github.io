b32eefbf07c15b22c7be97f3c63fcdb45f7fb22e
Longino Bigage <longinobigage@live.com>
2022-06-29 15:20:34 +0300

    Bran New

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    plugins: [
-        require("tailwindcss"), require("postcss-nested"), require("autoprefixer"), ...(
-            process.env.NODE_ENV === "production" ?
-                [
-                    require("cssnano")({
-                        preset: "default",
-                    }),
-                ] :
-                []
-        ),
-    ],
-};
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,112 +1,51 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
-    purge: {
-        content: ["**/*.html"],
-        options: {
-            safelist: {
-                standard: ["active"],
-                deep: [/glide/]
-            }
-        }
+  purge: {
+    content: ["./public/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      secondary: "#F4F2ED",
+      black: "black",
+      white: "white",
     },
-    darkMode: true,
-    theme: {
-        fontFamily: {
-            header: ['Raleway', "sans-serif"],
-            body: ['Open Sans', "sans-serif"]
-        },
-
-        screens: {
-            xs: "375px",
-            ...defaultTheme.screens
-        },
-
-        colors: {
-            transparent: "transparent",
-            primary: "#ffffff",
-            secondary: "#252426",
-            white: '#ffffff',
-            black: "#000000",
-            yellow: "#f9e71c",
-            lila: "#e6e5ec",
-            "grey-10": "#6c6b6d",
-            "grey-20": "#7c7c7c",
-            "grey-30": "#919091",
-            "grey-40": "#929293",
-            "grey-50": "#f4f3f8",
-            "grey-60": "#edebf6",
-            "grey-70": "#d8d8d8",
-            "hero-gradient-from": "rgba(85, 64, 174, 0.95)",
-            "hero-gradient-to": "rgba(65, 47, 144, 0.93)",
-            "blog-gradient-from": "#8f9098",
-            "blog-gradient-to": "#222222"
-        },
-
-        container: {
-            center: true,
-            padding: "1rem"
-        },
-
-        shadows: {
-            default: '0 2px 18px rgba(0, 0, 0, 0.06)',
-            md: '0 -3px 36px rgba(0, 0, 0, 0.12)',
-        },
-
-        extend: {
-            spacing: {
-                "13": '3.25rem',
-                "15": '3.75rem',
-                "17": '4.25rem',
-                "18": '4.5rem',
-                "19": '4.75rem',
-                "42": '10.5rem',
-                "76": "19rem",
-                "84": "21rem",
-                "88": "22rem",
-                "92": "23rem",
-                "100": "25rem",
-                "104": "26rem",
-                "108": "27rem",
-                "112": "28rem",
-                "116": "29rem",
-                "120": "30rem",
-                "124": "31rem",
-                "128": "32rem",
-                "132": "33rem",
-                "136": "34rem",
-                "140": "35rem",
-                "144": "36rem",
-                "148": "37rem",
-                "152": "38rem",
-                "156": "39rem",
-                "160": "40rem",
-                "164": "41rem",
-                "168": "42rem",
-                "172": "43rem",
-                "176": "44rem",
-                "180": "45rem",
-                "184": "46rem",
-                "188": "47rem",
-                "190": "48rem",
-                "194": "49rem",
-                "200": "50rem",
-                "204": "51rem"
-            },
-            zIndex: {
-                "-1": "-1"
-            },
-            inset: {
-                '2/5': '40%'
-            }
-        }
+    fontFamily: {
+      "pt-serif": ["PT Serif", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
     },
-    variants: {
-        extend: {
-            display: ['responsive', 'group-hover'],
-        }
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "100%": "100%",
     },
-    plugins: [require("@tailwindcss/typography"), require(
-        "@tailwindcss/forms"), require(
-            "@tailwindcss/aspect-ratio"),]
+    extend: {
+      backgroundImage: {
+        underline1: "url('./assets/Underline1.svg')",
+        underline2: "url('./assets/Underline2.svg')",
+        underline3: "url('./assets/Underline3.svg')",
+        underline4: "url('./assets/Underline4.svg')",
+        highlight3: "url('./assets/Highlight3.svg')",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
